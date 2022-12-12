7fb0adaf903b7c8a95486032ab9f0f20aade46d7
uncleboss12 <justineobiazi@gmail.com>
2022-12-12 08:58:16 -0800

    updates from my development

---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+    <header>
+      <h1>{{ title }}</h1>
+      <Button v-show="homePage"
+        @btn-click="$emit('toogle-add-task')" 
+        v-bind:text="showAddTask ? 'Close': 'Add Task'" 
+        :color="showAddTask ? 'red': 'green'"
+      />
+    </header>
+</template>
+
+<script>
+import Button from './Button.vue'
+export default {
+        name: "Header",
+        props: {
+            title: String,
+            showAddTask: Boolean,
+    },
+    components: {
+      Button
+    },
+    computed: {
+      homePage() {
+        if (this.$route.path === '/') {
+          return true;
+      } else { return false; }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+  }
+</style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,44 @@
+<template>
+    <div v-bind:key='task.id' v-for="task in tasks">
+        <Task @toggle-reminder="$emit('toggle-reminder', task.id)" @delet-task="$emit('delete-task', task.id)" :task="task"/>
+    </div>
+    
+</template>
+
+<script>
+import Task from './Task'
+
+export default {
+   name: 'Tasks',
+   props: {
+      tasks: Array,
+      },
+    
+    components : {
+        Task,
+    }, 
+    emits: ['delete-task', 'toogle-reminder'],
+        
+}
+</script>
+
+<style scoped>
+.task {
+  background: #f4f4f4;
+  margin: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+
+.task.reminder {
+  border-left: 5px solid green;
+}
+
+.task h3 {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
+
+
